2f67e5be0d06d18e9c891d5c7975a8fde1cdbde4
FSanchez <anfelsanchez@gmail.com>
2019-09-07 00:05:46 -0500

    Primera implementación

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
 
+  arrayNames: string[];
+  outputName: string = "";
+  textedNames: string = "";
+  interval;
+
+  constructor() { }
+
+  async rollClick() {
+    let n = 6;
+
+    this.arrayNames = this.textedNames.split(",");
+    let maxNumber = this.arrayNames.length; - 1;
+    this.interval = setInterval(() => {
+      this.outputName = this.arrayNames[Math.floor(Math.random() * maxNumber)];
+      n--;
+      if (n < 0 && this.interval) {
+        clearInterval(this.interval);
+      }
+    }, 500)
+  }
 }
